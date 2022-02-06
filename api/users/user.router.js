6ca30fb77f008fb6bb3ba23411d6dda952c31e83
SUMANTA KABIRAJ <infysumanta@gmail.com>
2022-02-07 00:43:20 +0530

    Adding login with JWT

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -4,13 +4,17 @@ const {
   getUsers,
   updateUsers,
   deleteUser,
+  login,
 } = require("./user.controller");
 
 const router = require("express").Router();
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUsersByUserId);
-router.patch("/", updateUsers);
-router.delete("/", deleteUser);
 
+const { checkToken } = require("../../auth/token_validation");
+
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUsersByUserId);
+router.patch("/", checkToken, updateUsers);
+router.delete("/", checkToken, deleteUser);
+router.post("/login", login);
 module.exports = router;
--- api/users/user.services.js
+++ api/users/user.services.js
@@ -75,4 +75,17 @@ module.exports = {
       }
     );
   },
+
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from registration where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
 };
